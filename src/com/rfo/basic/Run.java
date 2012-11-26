5e58ceb892b74b7c5197d4d73a7ab0468583fa2d
RFO-BASIC <basic@laughton.com>
2012-11-26 04:15:16 -0800

    Clean up the Tone command. Take out the clicks by ramping amplitude up and down. Correct the play duration.

--- src/com/rfo/basic/Run.java
+++ src/com/rfo/basic/Run.java
@@ -9139,21 +9139,18 @@ private boolean doUserFunction(){
 	  
 	  private boolean executeTONE(){
 		  
-		    double duration = 1; // seconds
-		    double freqOfTone = 1000; // hz
-//		    int sampleRate = 44100;
-		    int sampleRate = 8000;
+		    double duration = 1; 				// seconds
+		    double freqOfTone = 1000; 			// hz
+		    int sampleRate = 8000;				// a number
 
-		  if (!evalNumericExpression())return false;							// Get frequency
+		  if (!evalNumericExpression())return false;			// Get frequency
 		  freqOfTone =  EvalNumericExpressionValue;
 		  
-		  if (ExecutingLineBuffer.charAt(LineIndex)!=','){SyntaxError(); return false;} 
-		  ++LineIndex;
+		  if (!isNext(',')) return false;
 
-		  if (!evalNumericExpression())return false;							// Get duration
-		  duration= EvalNumericExpressionValue/1000;
+		  if (!evalNumericExpression())return false;			// Get duration
+		  duration= EvalNumericExpressionValue/1000 ;
 	
-		  
 	    	double dnumSamples = duration * sampleRate;
 	    	dnumSamples = Math.ceil(dnumSamples);
 	    	int numSamples = (int) dnumSamples;
@@ -9178,9 +9175,10 @@ private boolean doUserFunction(){
 	    		}
 	    	}
 
-	        // fill out the array
-	        for (int i = 0; i < numSamples; ++i) {
-	            sample[i] = Math.sin(2 * Math.PI * i / (sampleRate/freqOfTone));
+	    	if (!checkEOL()) return false;				// No more parameters expected
+	    	
+	        for (int i = 0; i < numSamples; ++i) {		// Fill the sample array
+	        	sample[i] = Math.sin(freqOfTone * 2 * Math.PI * i / (sampleRate));
 	        }
 
 	        // convert to 16 bit pcm sound array
@@ -9188,34 +9186,62 @@ private boolean doUserFunction(){
 	        // convert to 16 bit pcm sound array
 	        // assumes the sample buffer is normalised.
 	        int idx = 0;
-	        for (final double dVal : sample) {
-	            // scale to maximum amplitude
+	        int i = 0 ;
+	        
+	        int ramp = numSamples / 20 ;									// Amplitude ramp as a percent of sample count
+	       
+	        
+	        for (i = 0; i< ramp; ++i) {										// Ramp amplitude up (to avoid clicks)
+	        	double dVal = sample[i];
+	        																// Ramp up to maximum
+	            final short val = (short) ((dVal * 32767 * i/ramp));
+	            															// in 16 bit wav PCM, first byte is the low order byte
+	            generatedSnd[idx++] = (byte) (val & 0x00ff);
+	            generatedSnd[idx++] = (byte) ((val & 0xff00) >>> 8);
+	        }
+
+	        
+	        for (i = i; i< numSamples - ramp; ++i) {						// Max amplitude for most of the samples
+	        	double dVal = sample[i];
+	        																// scale to maximum amplitude
 	            final short val = (short) ((dVal * 32767));
-	            // in 16 bit wav PCM, first byte is the low order byte
+	            															// in 16 bit wav PCM, first byte is the low order byte
 	            generatedSnd[idx++] = (byte) (val & 0x00ff);
 	            generatedSnd[idx++] = (byte) ((val & 0xff00) >>> 8);
 	        }
 	        
-	        AudioTrack audioTrack = null;
+	        for (i = i; i< numSamples; ++i) {								// Ramp amplitude down
+	        	double dVal = sample[i];
+	        																// Ramp down to zero
+	            final short val = (short) ((dVal * 32767 * (numSamples-i)/ramp ));
+	            															// in 16 bit wav PCM, first byte is the low order byte
+	            generatedSnd[idx++] = (byte) (val & 0x00ff);
+	            generatedSnd[idx++] = (byte) ((val & 0xff00) >>> 8);
+	        }
+	        
+	        AudioTrack audioTrack = null;									// Get audio track
 	        try {
 	        	audioTrack = new AudioTrack(AudioManager.STREAM_MUSIC,
 	        			sampleRate, AudioFormat.CHANNEL_CONFIGURATION_MONO,
-	        			AudioFormat.ENCODING_PCM_16BIT, (int)numSamples,
+	        			AudioFormat.ENCODING_PCM_16BIT, (int)numSamples*2,
 	        			AudioTrack.MODE_STATIC);
-	        	audioTrack.write(generatedSnd, 0, generatedSnd.length);
-	        	audioTrack.play();
+	        	audioTrack.write(generatedSnd, 0, generatedSnd.length);		// Load the track
+	        	audioTrack.play();											// Play the track
 	        }
 	        catch (Exception e){
 	        	RunTimeError("Error: " + e);
 	        	return false;
 	        }
+	        
 	        int x =0;
-	        do{
+	        do{														// Montior playback to find when done
 	        	 if (audioTrack != null) 
 	        		 x = audioTrack.getPlaybackHeadPosition(); 
-	        	 else x = numSamples;	        }while (x<numSamples/2);
+	        	 else 
+	        		 x = numSamples;	        
+	        }while (x<numSamples);
 	        
-	        if (audioTrack != null) audioTrack.release();
+	        if (audioTrack != null) audioTrack.release();			// Track play done. Release track.
 
 	        return true;
 	  }
