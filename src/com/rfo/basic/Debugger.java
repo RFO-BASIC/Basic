a0c7693c0b4e4f403a3f2588ea01898b236e5b6b
jMarcS <jMarcS@github>
2013-10-23 10:42:04 -0600

    Remove ACRA, at least for now. See Issue #110.

--- src/com/rfo/basic/Debugger.java
+++ src/com/rfo/basic/Debugger.java
@@ -1,42 +1,10 @@
 package com.rfo.basic;
 
-import org.acra.ACRA;
-import org.acra.annotation.ReportsCrashes;
-import org.acra.ReportingInteractionMode;
-
-
-import android.app.Application;
-
-/* This code was created by the Android development team. 
- * When BASIC! crashes, it will send a fairly complete crash
- * report to a Google Docs spread sheet. This report can then
- * be examined for debug purposes. 
- * 
- * See http://acra.ch/ for more details
- * 
+/* Through BASIC! version v01.75.02, this file held the Application sub-class
+ * that initialized the Application Crash Reports for Android (ACRA).
+ * To reinstall the crash reporter, restore earlier versions of this file
+ * and of the AndroidManifest.xml file. There is also a Preferences option
+ * to disable crash reporting, found only in the V01.75.02 version of
+ * the res/xml/settings.xml file.
  */
-//@ReportsCrashes(formKey = "dDQ4cXhUQjdJbTZrUzlUN3NsWDctdWc6MQ" ,
-@ReportsCrashes(formKey = "dGY1MVdrZUljVmxUelZCekJyUkwwQ3c6MQ" ,
-		mode = ReportingInteractionMode.NOTIFICATION,
-//		forceCloseDialogAfterToast = true,
-      resToastText = R.string.crash_toast_text, // optional, displayed as soon as the crash occurs, before collecting data which can take a few seconds
-        resNotifTickerText = R.string.crash_notif_ticker_text,
-        resNotifTitle = R.string.crash_notif_title,
-        resNotifText = R.string.crash_notif_text,
-        resNotifIcon = android.R.drawable.stat_notify_error, // optional. default is a warning sign
-        resDialogText = R.string.crash_dialog_text,
-        resDialogIcon = android.R.drawable.ic_dialog_info, //optional. default is a warning sign
-        resDialogTitle = R.string.crash_dialog_title, // optional. default is your application name
-        resDialogCommentPrompt = R.string.crash_dialog_comment_prompt, // optional. when defined, adds a user text field input with this text resource as a label
-        resDialogOkToast = R.string.crash_dialog_ok_toast // optional. displays a Toast message when the user accepts to send a report.
-        )
-public class Debugger extends Application {
-	
-    @Override
-    public void onCreate() {
-        // The following line triggers the initialization of ACRA
-        ACRA.init(this);
-        super.onCreate();
-    }
-}
 
